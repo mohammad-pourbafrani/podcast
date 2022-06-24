9e5f75e2950a22c73fd925caf6c6ee23baa57580
mohammad-pourbafrani <m.pourbafarani@gmail.com>
2022-06-24 15:27:30 +0430

    add page main screen

---
+++ lib/model/bottom_navication_model.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+List<Widget> bottomNavicationIconList = [
+  const Icon(
+    Icons.home,
+    size: 32,
+  ),
+  const Icon(
+    Icons.favorite,
+    size: 32,
+  ),
+  const Icon(
+    Icons.add,
+    size: 32,
+  ),
+  const Icon(
+    Icons.supervised_user_circle_rounded,
+    size: 32,
+  ),
+];
---
+++ lib/view/main_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:podcast/model/bottom_navication_model.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({Key? key}) : super(key: key);
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  var indexBottomNav = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
