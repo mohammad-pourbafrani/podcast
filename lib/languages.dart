f1343b1c9daed762746e427e272e861c410fe1f3
mohammad-pourbafrani <m.pourbafarani@gmail.com>
2022-06-24 17:37:14 +0430

    use multiplate languages and localization

---
+++ lib/languages.dart
@@ -0,0 +1,9 @@
+import 'package:get/get.dart';
+
+class Languages extends Translations {
+  @override
+  Map<String, Map<String, String>> get keys => {
+        'en_US': {'': ''},
+        'fa_IR': {'': ''}
+      };
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:podcast/languages.dart';
 import 'package:podcast/view/main_screen.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,8 +14,20 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const GetMaterialApp(
-      home: MainScreen(),
+    return GetMaterialApp(
+      translations: Languages(),
+      locale: Get.deviceLocale,
+      fallbackLocale: const Locale('en', 'US'),
+      localizationsDelegates: const [
+        GlobalCupertinoLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      // supportedLocales: const [
+      //   Locale("en", "US"),
+      //   Locale("fa", "IR"),
+      // ],
+      home: const MainScreen(),
     );
   }
 }
